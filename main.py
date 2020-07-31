852a23cd0ddc0f692f07753655a2a87c8d270d61
Leon <justleonliu@gmail.com>
2020-07-31 04:09:37 +0000

    Change syntax

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ bmi = weight/(height**2)
 
 
 if bmi < 18.5 :
-  print('Your below normal')
-elif bmi >= 18.5 and bmi <= 25 :
+  print('Your are below normal')
+elif bmi >= 18.5 and bmi < 25 :
   print('Your are normal')
-elif bmi >= 25 and bmi <= 30 :
+elif bmi >= 25 and bmi < 30 :
   print('Your are overweight')
-elif bmi >= 30 and bmi <= 40 :
+elif bmi >= 30 and bmi < 40 :
   print('Your are badly overweight')
 elif bmi >= 40 :
   print('Your are extremly overweight')
