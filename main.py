f96c260dea93ba3a5fe582b31160fba4b6bf8bae
Leon <justleonliu@gmail.com>
2020-07-31 04:07:54 +0000

    Add conditions under 18.5

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ bmi = weight/(height**2)
 
 
 
-
-if bmi >= 18.5 and bmi <= 25 :
+if bmi < 18.5 :
+  print('Your below normal')
+elif bmi >= 18.5 and bmi <= 25 :
   print('Your are normal')
 elif bmi >= 25 and bmi <= 30 :
   print('Your are overweight')
